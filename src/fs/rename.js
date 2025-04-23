439f68d7f4bce7913077b2de6bd97c9fec90144b
Duude92 <switch4acc@gmail.com>
2025-04-23 14:23:41 +0300

    Implement rename method

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
+import fs, {existsSync} from 'node:fs'
+import {fileURLToPath} from "url";
+import {dirname, join} from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileDirname = join(__dirname, 'files');
+const srcPath = join(fileDirname, 'wrongFilename.txt');
+const destPath = join(fileDirname, 'properFilename.md');
+const sourceExists = existsSync(srcPath);
+const destExists = existsSync(destPath);
+
 const rename = async () => {
-    // Write your code here 
+    // Write your code here
+    if(!sourceExists || destExists) {
+        throw new Error('FS operation failed');
+    }
+    await fs.rename(srcPath, destPath, (err) => {
+        if(err) {
+            throw err;
+        }
+        console.log('File renamed successfully!');
+    } )
 };
 
 await rename();
