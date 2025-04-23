5312cb8705af16df07750432fb9a4f0b90c3cd0a
Duude92 <switch4acc@gmail.com>
2025-04-23 13:39:47 +0300

    Implement copy method

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import {existsSync} from 'node:fs';
+import {cp} from 'node:fs/promises';
+import {fileURLToPath} from 'url';
+import {dirname, join} from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const sourceDir = join(__dirname, 'files');
+const destDir = join(__dirname, 'files_copy');
+const sourceExists = existsSync(sourceDir);
+const destExists = existsSync(destDir);
+
 const copy = async () => {
     // Write your code here 
+    if(!sourceExists || destExists) {
+        throw new Error('FS operation failed');
+    }
+    cp(sourceDir, destDir, {recursive: true})
+        .then(() => {
+            console.log('Files copied successfully!');
+        })
+        .catch((error) => {
+            console.error('Error copying files:', error);
+        });    
 };
 
 await copy();
