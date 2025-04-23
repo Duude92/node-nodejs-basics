e1f322469ab8dd3dfb2afd8f8d30243194c0e428
Duude92 <switch4acc@gmail.com>
2025-04-23 18:21:46 +0300

    Implementation of decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import {fileURLToPath} from "url";
+import path from "path";
+import zlib from "zlib";
+import fs from "node:fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const destName = path.join(__dirname, 'files', 'fileToCompress.txt');
+const srcName = path.join(__dirname, 'files', 'archive.gz');
+
 const decompress = async () => {
-    // Write your code here 
+    // Write your code here
+    let fileStream = fs.createReadStream(srcName);
+    let destStream = fs.createWriteStream(destName);
+    fileStream.pipe(zlib.createGunzip()).pipe(destStream).on("finish", () => console.log("File compressed."));
 };
 
 await decompress();
