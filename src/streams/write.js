96258f0d84003c990ee2549a1b0aa15316094515
Duude92 <switch4acc@gmail.com>
2025-04-23 17:08:23 +0300

    Implementation of writeableStream task module

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import {fileURLToPath} from "url";
+import path from "path";
+import fs from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileName = path.join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    // Write your code here
+    let fileStream = fs.createWriteStream(fileName);
+    process.stdin.pipe(fileStream);
 };
 
 await write();
