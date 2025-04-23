859805a39db191e6cdd8e98757a5807810aa5db0
Duude92 <switch4acc@gmail.com>
2025-04-23 14:49:13 +0300

    Implement list method

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import {fileURLToPath} from "url";
+import {dirname, join} from "path";
+import fs from "node:fs"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileDirname = join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+    // Write your code here
+    if(!fs.existsSync(fileDirname)) throw new Error('FS operation failed');
+    fs.readdir(fileDirname, (err, files) => {
+        if(err) throw err;
+        console.log(files);
+    })
 };
 
 await list();
