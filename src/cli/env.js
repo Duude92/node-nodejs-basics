5523cab9563805b5298ce850b0eef64192997179
Duude92 <switch4acc@gmail.com>
2025-04-23 15:17:39 +0300

    Implement env module

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    console.log(Object.entries(process.env)
+        .filter(([key,value], index) => key.startsWith('RSS_'))
+        .map(([key,value],index) => `${key}=${value}` )
+        .join('; '));
 };
 
 parseEnv();
