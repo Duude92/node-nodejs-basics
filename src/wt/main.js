18a9d6afa23bf5d7fa5a5cf966593e315a42e62b
Duude92 <switch4acc@gmail.com>
2025-04-23 19:26:16 +0300

    Implementation of working thread task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import os from "node:os";
+import {Worker, workerData} from "node:worker_threads";
+import {fileURLToPath} from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileName = path.join(__dirname, 'worker.js');
+
 const performCalculations = async () => {
     // Write your code here
+    let cpuCoreCount = os.cpus().length;
+    let workers = []
+    for (let i = 0; i < cpuCoreCount; i++) {
+        workers.push( new Promise((resolve) => {
+            const worker = new Worker(fileName, {workerData: (10 + i)})
+            worker.on('message', message => {
+                resolve({status: 'resolved', data: message});
+            })
+            worker.on('error', err => resolve({status: 'error', data: null}));
+        }));
+    }
+    let values = await Promise.all(workers);
+    console.log(values);
 };
 
 await performCalculations();
