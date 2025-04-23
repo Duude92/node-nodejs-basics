975dc6d1f0598df785240b15b41473c44b0ab781
Duude92 <switch4acc@gmail.com>
2025-04-23 16:41:06 +0300

    Implementation of calcHash module

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import crypto from 'node:crypto'
+import fs from "node:fs";
+import {fileURLToPath} from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileName = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    console.log(crypto.createHash('sha256')
+        .update(fs.readFileSync(fileName, 'utf8'))
+        .digest('hex'));
 };
 
 await calculateHash();
