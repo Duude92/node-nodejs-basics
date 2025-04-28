91b0e6b2dc4370aa4f3661e93167489e0f2d23ff
Duude92 <switch4acc@gmail.com>
2025-04-28 10:15:55 +0300

    Revert "Implementation of cjm2esm module"
    This reverts commit e0a2bb8028764ce3ab1bac13accbee3cd67f5e41.

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -16,9 +16,9 @@ if (random > 0.5) {
 } else {
     fileName = 'b.json';
 }
-
-unknownObject = fs.readFileSync(path.join(__dirname, 'files', fileName), 'utf-8');
-
+// Both variants are viable https://discord.com/channels/755676888680366081/1363206860160631114/1365003228994998403
+// unknownObject = {default: JSON.parse(fs.readFileSync(path.join(__dirname, 'files', fileName), 'utf-8'))};
+unknownObject = await import(`./files/${fileName}`, { with: { type: 'json' } });
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
@@ -31,8 +31,8 @@ const myServer = createServerHttp((_, res) => {
 });
 
 const PORT = 3000;
-
-console.log(unknownObject);
+// Assign to emp object prototype to get rid of [Module prototype null] or something
+console.log(Object.assign({}, unknownObject));
 
 myServer.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
