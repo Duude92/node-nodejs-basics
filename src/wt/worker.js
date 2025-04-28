df41e94c2be9fa312b9261b363370374ca5034c9
Duude92 <switch4acc@gmail.com>
2025-04-28 14:23:09 +0300

    Added commented simulation of worker failure

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -3,6 +3,9 @@ import worker from "node:worker_threads"
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
+    // This code is for simulation of Error behaviour
+    // let rand = Math.random()
+    // if(rand > 0.5) throw new Error('');
     // This function sends result of nthFibonacci computations to main thread
     worker.parentPort.postMessage(nthFibonacci(worker.workerData));
 };
